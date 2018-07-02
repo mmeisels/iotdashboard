35fa13c3be87a74d84879355301f77d9127f1cec
Michael Meisels <mmeisels@mmeisels-ltm2.internal.salesforce.com>
2018-07-02 22:00:36 +1200

    New CHange

--- src/main/java/com/iot/app/springboot/dashboard/IoTDataDashboard.java
+++ src/main/java/com/iot/app/springboot/dashboard/IoTDataDashboard.java
@@ -1,5 +1,7 @@
 package com.iot.app.springboot.dashboard;
 
+import org.springframework.boot.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
